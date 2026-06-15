b2236cf3e958f33c3cde69f9fcd5ba38fdb65f5a
valcrim <vladimir.vyaz1@gmail.com>
2026-06-16 02:09:28 +0500

    Переименование роутера booking -> slots

---
+++ src/coworking/api/slots.py
@@ -0,0 +1,56 @@
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException, status
+from services.repositories.roomsRepo import SlotRepository
+from dependencies.repositories import get_slot_repo
+from dependencies.types import OwnerOrAdmin, AdminUser
+
+
+router = APIRouter(prefix='/slots', tags=['slots'])
+
+
+@router.patch('/{slot_id}', tags=['slots', 'booking'])
+async def update_slot(
+        user: OwnerOrAdmin,
+        slot_id: int, 
+        repo: SlotRepository = Depends(get_slot_repo)
+        ):
+    """ 
+    Бронирование и снятие брони со слота.
+
+    Метод защищен.
+    Снять бронь может только владелец брони или администратор.
+    Свободные слот может забронировать любой пользователь.
+    """
+    slot = await repo.get_slot(slot_id)
+    if not slot:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                             "Slot with this id does not exist")
+    
+    if slot.booked_by is None:
+        booked_by = user.id
+    else:
+        booked_by = None
+    slot = await repo.book_slot(slot_id, booked_by)
+    return slot
+
+
+@router.delete("/{slot_id}")
+async def delete_slot(
+        user: AdminUser,
+        slot_id: int,
+        repo: SlotRepository = Depends(get_slot_repo)
+        ):
+    """ 
+    Удаление слота в комнате.
+    Метод защищен. Доступно только администраторам.
+    """
+    slot = await repo.get_slot(slot_id)
+    if not slot: 
+        raise HTTPException(status.HTTP_404_NOT_FOUND, 
+                             f"Slot with this id is does not exist")
+    if await repo.delete_slot(slot_id):
+        return {"detail": "Slot deleted successfully"}
+    else:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                             "Failed to delete the slot")
